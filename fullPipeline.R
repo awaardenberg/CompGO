48825b0c010107e2bc753304334701b7e331db1e
Sam Bassett <sam.d.bassett@gmail.com>
2014-01-03 11:35:59 +1100

    cleaned up for submission

--- fullPipeline.R
+++ fullPipeline.R
@@ -7,16 +7,6 @@ getUniqueGenes <- function(setA, setB) {
     return(uniqs)
 }
 
-# genes is a list of genes in MGI symbol format to be converted
-mgiToEntrez <- function(genes) {
-    require('biomaRt')
-    if (!exists('ensembl')) {
-        ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
-    }
-    ent <- getBM(attributes=c('mgi_symbol', 'entrezgene'), filters='mgi_symbol', values=genes, mart=ensembl)
-    return(ent)
-}
-
 # bed contains chromosomal regions plus chrX
 bedToEntrez <- function(bed) {
     require('biomaRt')
@@ -45,7 +35,7 @@ ucscDbDump <- function(db=NULL, session = NULL) {
     return(ucsc.table)
 }
 
-annotateFromDump <- function(path, db = NULL, sample = NULL, upstream = 0, downstream = 0) {
+annotateBedFromUCSC <- function(path, db = NULL, sample = NULL, upstream = 0, downstream = 0) {
     require('rtracklayer')
     if(is.null(db)) {
         message("grabbing db dump, may take time...")
@@ -111,157 +101,6 @@ annotateFromDump <- function(path, db = NULL, sample = NULL, upstream = 0, downs
     return(closestGenes)
 }
 
-bedToGeneUCSC <- function(path, upstream=0, downstream=0, geneToBed = FALSE, session = NULL) {
-    require(rtracklayer)
-    require(R.cache)
-    if (is.null(session)) {
-        message("requesting session...")
-        session = browserSession("UCSC")
-        genome(session) = 'mm9'
-        message("done!")
-    }
-    # set up ranges here from bed file
-    range = import.bed(path)
-    start(ranges(range)) = start(ranges(range)) - 10000
-    end(ranges(range)) = end(ranges(range)) + 10000
-
-    if (length(range) > 1000) {
-        x = length(range)
-        range2 = range[1000:x]
-        range = range[1:1000]
-    }
-    ucscTableQuery_mem = addMemoization(ucscTableQuery)
-    message("querying UCSC...")
-    key = list(track='refGene', range = range, table='refGene')
-    tableQuery = loadCache(key)
-    if(is.null(tableQuery)) {
-        #print("cache miss =(")
-        tableQuery = memoizedCall(what=ucscTableQuery_mem, session, track='refGene', range = range, table='refGene')
-        saveCache(tableQuery, key=key)
-    }
-    table = getTable(tableQuery)
-    #table= getTable(ucscTableQuery(session, track='refGene', range = range, table='refGene'))
-    if(exists('range2')) {
-        key = list(track='refGene', range = range2, table='refGene')
-        tableQuery = loadCache(key)
-        if(is.null(tableQuery)) {
-            #print("cache miss =(")
-            tableQuery = memoizedCall(what=ucscTableQuery_mem, session, track='refGene', range = range2, table='refGene')
-            saveCache(tableQuery, key=key)
-        }
-        table = rbind(table, getTable(tableQuery))
-        range = c(range, range2)
-    }
-    message("done!")
-# subtract 7kb from frame
-    #start(ranges(range)) = start(ranges(range)) + 7000
-    #end(ranges(range)) = end(ranges(range)) - 7000
-    bed = read.bed(path)
-    midpoints = data.frame(chr = paste("chr",bed$V1,sep=""), midpoint = (bed$V3+bed$V2)/2)
-    finalTable = data.frame()
-
-    #table = subset(table, grepl("NM_", name))
-    
-    #table = transform(table, txStart = ifelse(strand == '-', txEnd, txStart))
-    #table = transform(table, txEnd = ifelse(strand=='-', txStart, txEnd))
-    if (geneToBed) {
-        message("performing gene-to-bed analysis...")
-        for (i in 1:nrow(table)) {
-            chr_mid = subset(midpoints, chr = table[i, ]$chr)
-            if (table[i,]$strand == '+') {
-                min = which.min(abs(chr_mid$midpoint - table[i,]$txStart))
-            } else if (table[i,]$strand == '-') {
-                min = which.min(abs(chr_mid$midpoint - table[i,]$txEnd))
-            }
-            minRow = table[i,]
-            minRow = minRow[c('name2', 'chrom', 'txStart', 'txEnd', 'strand')]
-            if (table[i,]$strand == '+') {
-                minRow$distance = chr_mid[min,]$midpoint - minRow$txStart
-            } else if (table[i,]$strand == '-') {
-                minRow$distance = minRow$txEnd - chr_mid[min,]$midpoint
-            }
-            finalTable = rbind(finalTable, minRow)
-        }
-    } else {
-        message("performing bed-to-gene analysis...")
-        for (i in 1:nrow(midpoints)) {
-            chr_table = subset(table, chrom = midpoints[i, ]$chr)
-
-            plus = subset(chr_table, chr_table$strand == '+')
-            minus = subset(chr_table, chr_table$strand == '-')
-
-            plus$distance=  midpoints[i,]$midpoint - plus$txStart 
-            minus$distance = minus$txStart - midpoints[i, ]$midpoint 
-            minPlus = which.min(abs(plus$distance))
-            minMinus = which.min(abs(minus$distance))
-
-            if(plus[minPlus,]$distance <= minus[minMinus,]$distance) {
-                minRow = plus[minPlus, ]
-            } else {
-                minRow = minus[minMinus, ]
-            }
-
-            #minRow = chr_table[min,]
-            #minRow = minRow[c('name2', 'chrom', 'txStart', 'txEnd', 'strand')]
-
-            finalTable = rbind(finalTable, minRow)
-        }
-    }
-    message('done!')
-    plot(density(subset(finalTable$distance, abs(finalTable$distance) < 10000), bw=400))
-
-    return(finalTable)
-}
-
-
-# for each chromosomal region, find the closest transcript_start and calculate absolute distance from middle.
-bedToGeneDistance <- function(bed, upstream=0, downstream=0, verbose = FALSE) {
-    if (!exists('ensembl')) {
-        ensembl <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
-    }
-    bed$V3 = bed$V3 + downstream
-    bed$V2 = bed$V2 - upstream
-    mid = data.frame(chr = bed$V1, midpoint = bed$V3 - (bed$V3 - bed$V2)/2)
-    #mid$chr = bed$V1
-    #mid = bed$V2
-    regions = paste(bed$V1, bed$V2, bed$V3, sep=":")
-    message("Querying...")
-    query <- getBM(attributes=c('chromosome_name', 'start_position', 'end_position', 'strand', 'mgi_symbol'),
-        filters = 'chromosomal_region', values = (regions), mart=ensembl, verbose = verbose)
-    #print(head(query))
-    #print(str(query))
-    #query <- getBM_dump(attributes=c('transcript_start', 'mgi_symbol'), filters = 'chromosomal_region', values = (regions), mart=ensembl, verbose = verbose)
-    #return(query)
-    message("Done!")
-    final = data.frame()
-    for(i in 1:nrow(query)) {
-        mid_chr = subset(mid, chr = query[i,]$chromosome_name)
-        if (query[i,]$strand == 1) {
-            min = which.min(abs(query[i,]$start_position - mid$midpoint))
-            result = query[i,]
-            #result = mid_chr[min,]
-            result$distance = mid_chr[min,]$midpoint - query[i,]$start_position
-        } else if (query[i,]$strand == -1) {
-            min = which.min(abs(query[i,]$end_position - mid$midpoint))
-            result = query[i,]
-            #result = mid_chr[min,]
-            result$distance = mid_chr[min,]$midpoint - query[i,]$end_position
-        }
-        final = rbind(final, result)
-    }
-        
-    "'
-    for (i in 1:nrow(mid)) {
-        chr_query = subset(query, chromosome_name = mid[i,]$chr)
-        min = which.min(abs(chr_query$transcript_start - mid[i,]$midpoint))
-        result = chr_query[min,]
-        result$distance = (result$transcript_start - mid[i,]$midpoint)
-        final = rbind(final, result)
-    }
-    '"
-    return(final)
-}
-
 read.bed <- function(path) {
     bed <- read.table(path)
     bed$V1 = sub(pattern="chr", replacement="", bed$V1)
@@ -283,52 +122,6 @@ getFnAnot_genome <- function(entrezlist, david = NULL, email = NULL, idType = "E
     fnAnot <- getFunctionalAnnotationChart(david)
     return(fnAnot)
 }
-    # generate clusterProfiler images/pdfs in current directory comparing BP, MF, CC of two gene sets, A and B
-getClusterProfilerImages <- function(setA, setB, numCategories=25, organism="mouse", width=25, height=25) {
-    require('clusterProfiler')
-    setList <- list(setA = setA, setB = setB)
-# produce images for ontologies, advised not to use for loop
-    pdf("./automatically_generated_ontology.pdf")
-
-    #print("Generating BP...")
-    cmpList <- compareCluster(setList, fun="enrichGO", organism=organism, ont="BP")
-    plot(cmpList, showCategory=numCategories)
-
-    #print("Generating MF...")
-    cmpList <- compareCluster(setList, fun="enrichGO", organism=organism, ont="MF")
-    plot(cmpList, showCategory=numCategories)
-
-    #print("Generating CC...")
-    cmpList <- compareCluster(setList, fun="enrichGO", organism=organism, ont="CC")
-    plot(cmpList, showCategory=numCategories)
-    dev.off()
-}
-
-compareDavidEnrichmentBackground <- function(geneSet, background, email, geneType="ENTREZ_GENE_ID") {
-    require('RDAVIDWebService')
-    david <- DAVIDWebService$new(email=email)
-    addList(david, geneSet, idType=geneType, listType="Gene", listName="auto_list")
-    if (exists("background")) {
-        addList(david, geneSet, idType=geneType, listType="Background", listName="auto_bg")
-    }
-    setAnnotationCategories(david, c("GOTERM_BP_ALL", "GOTERM_MF_ALL", "GOTERM_CC_ALL"))
-    fnAnot <- getFunctionalAnnotationChart(david)
-
-    goDag <- DAVIDGODag(fnAnot, pvalueCutoff=0.05, 'BP')
-    pdf("./auto_generated_DAVID_BP.pdf")
-    plotGOTermGraph(g=goDag(goDag), r = goDag, node.shape='box')
-    dev.off()
-
-    goDag <- DAVIDGODag(fnAnot, pvalueCutoff=0.05, 'MF')
-    pdf("./auto_generated_DAVID_MF.pdf")
-    plotGOTermGraph(g=goDag(goDag), r = goDag, node.shape='box')
-    dev.off()
-
-    goDag <- DAVIDGODag(fnAnot, pvalueCutoff=0.05, 'CC')
-    pdf("./auto_generated_DAVID_CC.pdf")
-    plotGOTermGraph(g=goDag(goDag), r = goDag, node.shape='box')
-    dev.off()
-}
 
 plotPairwise <- function(setA, setB, benjaminiCutoff = NULL, pvalueCutoff = NULL, plotNA=FALSE, model='lm') {
     require('RDAVIDWebService')
@@ -346,8 +139,6 @@ plotPairwise <- function(setA, setB, benjaminiCutoff = NULL, pvalueCutoff = NULL
         setB_ben = setB$PValue
         names(setB_ben) = setB$Term
     }
-    #setA_comp = as.data.frame(setA_ben)
-    #setB_comp = as.data.frame(setB_ben)
     setA_comp = cbind(read.table(text=names(setA_ben)), setA_ben)
     setB_comp = cbind(read.table(text=names(setB_ben)), setB_ben)
     if(!is.null(benjaminiCutoff) || !is.null(pvalueCutoff)) {
@@ -363,10 +154,8 @@ plotPairwise <- function(setA, setB, benjaminiCutoff = NULL, pvalueCutoff = NULL
     corr = cor(-log10(comp$setA_ben), -log10(comp$setB_ben))
     corr = format(round(corr, 4), nsmall=4)
     print(corr)
-    #l = lm(-log10(setB_ben) ~ -log10(setA_ben), data=comp)
     #print(summary(l))
     p = ggplot(comp, aes(-log10(setA_ben), -log10(setB_ben)))
-    #p = ggplot(comp, aes((setA_ben), (setB_ben)))
     p + geom_point() + geom_smooth(method=model) + geom_text(data = NULL, x = 5, y=12, label=paste("cor:", corr, sep=' '))
 }
 
@@ -376,4 +165,91 @@ extractGOFromAnnotation <- function(fnAnot) {
         sub("(GO:[^~]+)~.*$","\\1", x)
     })
     return(fnAnot)
+}
+
+plotTwoGODags <- function (g1, g2, r1 = NULL, r2 = NULL, add.counts = TRUE, max.nchar = 60, node.colors = c(sig1 = "red", 
+    sig2 = "lightgreen", both="yellow", not = "white"), relaxPvals = FALSE, node.shape = "box", showBonferroni = FALSE, ...) 
+{
+
+    if (!require("Rgraphviz", quietly = TRUE)) 
+        stop("The Rgraphviz package is required for this feature")
+# get three sets of GO terms, one for each and then a union
+    n1 = nodes(g1)
+    n2 = nodes(g2)
+    nall = union(n1, n2)
+# gets term labels, if not present the ids are used
+    termLab <- if ("term" %in% union(names(nodeDataDefaults(g1)), names(nodeDataDefaults(g2)))) {
+        unlist(union(nodeData(g1, attr = "term"), nodeData(g2, attr = "term")))
+    }
+    else nall
+    if(length(termLab) != length(nall)) {
+        termLab = nall
+    }
+# applies a substring if the user supplies a max term length
+    if (!is.null(max.nchar)) 
+        termLab <- sapply(termLab, substr, 1L, max.nchar, USE.NAMES = FALSE)
+# creates vector with length number of nodes and values "white" (by default) for colouring
+    ncolors <- rep(node.colors["not"], length(nall))
+    if (!is.null(r1) && !is.null(r2) && add.counts) {
+# checks values of node colour argument
+        if (is.null(names(node.colors)) || !all(c("sig1","sig2","both","not") %in% 
+            names(node.colors))) 
+            stop(paste("invalid node.colors arg:", "must have named elements 'sig1', 'sig2', 'both' and 'not'"))
+# extracts goterms with pvalues from r
+        resultTerms1 <- names(pvalues(r1))
+        resultTerms2 <- names(pvalues(r2))
+        resultTermsAll = union(resultTerms1, resultTerms2)
+# if n is a significant term in r, give ncolors the sig. colour, else not sig. colour
+        if (relaxPvals) {
+            ncolors <-  ifelse(!nall %in% union(sigCategories(r1), sigCategories(r2)), node.colors["not"], 
+                        ifelse(nall %in% intersect(names(pvalues(r1)), names(pvalues(r2))), node.colors["both"], 
+                        ifelse(nall %in% names(pvalues(r1)), node.colors["sig1"], 
+                        ifelse(nall %in% names(pvalues(r2)), node.colors["sig2"],
+                        node.colors["not"]))))
+        } else {
+            ncolors <- ifelse(nall %in% intersect(sigCategories(r1), sigCategories(r2)), node.colors["both"], 
+                ifelse(nall %in% sigCategories(r1), node.colors["sig1"], ifelse(nall %in% sigCategories(r2), node.colors["sig2"],
+                node.colors["not"])))
+        }
+# annotate each node with a count as well
+        counts <- sapply(nall, function(x) {
+            if (x %in% intersect(resultTerms1, resultTerms2)) {
+                if (!showBonferroni) {
+                    paste(geneCounts(r1)[x], ", ", geneCounts(r2)[x], sep = "")
+                } else {
+                    paste(bonferronis(r1)[x], ", ", bonferronis(r2)[x], sep = "")
+                }
+                #paste(geneCounts(r1)[x]+geneCounts(r2)[x],"/",universeCounts(r1)[x]+universeCounts(r2)[x],
+                    #sep="")
+            } else if (x %in% resultTerms1) {
+                paste(geneCounts(r1)[x], "/", universeCounts(r1)[x], 
+                  sep = "")
+            } else if (x %in% resultTerms2) {
+                paste(geneCounts(r2)[x], "/", universeCounts(r2)[x], 
+                  sep = "")
+            } else {
+                "0/??"
+            }
+        })
+# put together the labels and counts for each go term
+        nlab <- paste(termLab, counts)
+    }
+    else {
+        nlab <- termLab
+    }
+
+    print("both: ")
+    print(length(grep("yellow", ncolors)))
+    print("r1: ")
+    print(length(grep("red", ncolors)))
+    print("r2: ")
+    print(length(grep("lightgreen", ncolors)))
+    print("neither: ")
+    print(length(grep("white", ncolors)))
+# uses Rgraphviz
+# for each node in g, give it label nlab etc...
+    nattr <- makeNodeAttrs(join(g1, g2), label = nlab, shape = node.shape, 
+        fillcolor = ncolors, fixedsize = FALSE)
+# plot the tree!
+    plot(join(g1,g2), ..., nodeAttrs = nattr)
 }

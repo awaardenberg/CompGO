b66ffcea2f4e6ceb1443586bead1ad80d8d9338d
Sam Bassett <sam.d.bassett@gmail.com>
2014-01-09 10:39:08 +1100

    formatting plot

--- EnrichmentVisualiser/R/fullPipeline.R
+++ EnrichmentVisualiser/R/fullPipeline.R
@@ -222,7 +222,16 @@ plotPairwise <- function(setA, setB, cutoff = NULL, useRawPvals = FALSE, plotNA=
     } else {
         p = ggplot(comp, aes(-log10(comp$setA_val), -log10(comp$setB_val)))
     }
-    p + geom_point() + geom_smooth(method=model) + geom_text(data = NULL, x = 5, y=9, label=paste("cor:", corr, sep=' '))
+    print(str(comp))
+    #p + geom_point() + geom_smooth(method=model) + geom_text(data = NULL, x = 5, y=9, label=paste("cor:", corr, sep=' '))
+    p = p + geom_point(aes(colour=comp$jaccard), size=2) + theme(axis.text.x=element_text(size=6), axis.text.y=element_text(size=6),
+        axis.line=element_line(), axis.title=element_text(size=6, face="bold"), legend.text=element_text(size=6), legend.title=element_text(size=6))
+
+    p = p + scale_colour_gradient2(expression(over(abs(paste("A", intersect(), "B")), abs(paste("A", union(), "B")))),
+        low="red", mid="red", high="blue", limits=c(0, 1))
+    p = p + annotate("text", label = paste("R=", corr), x = Inf, hjust = 1, y = Inf, vjust = 5, size = 2, colour = "black")
+    plot(p)
+    return(p)
 }
 
 extractGOFromAnnotation <- function(fnAnot) {

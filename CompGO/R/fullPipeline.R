d8b15a3c3714cfdbb85582a5d9b1b5dee477bbf1
Sam Bassett <sam.d.bassett@gmail.com>
2014-02-10 12:49:54 +1100

    Started implementing Marc's improvements

--- CompGO/R/fullPipeline.R
+++ CompGO/R/fullPipeline.R
@@ -1,177 +1,33 @@
 # R pipeline for GO analysis and gene annotation
-# by Sam Bassett, VCCRI, 2014
-
-#' @title Dump the UCSC database for a specified genome
-#' @description This function downloads a full dump of the specified genome in the specified format and returns it as a data.frame
-#' @param session If a session has already been requested, it can be passed into the function here
-#' @param genome The genome to grab from UCSC, default 'mm9' for mmusculus
-#' @param format The format, or track, to download from UCSC. Default refGene
-#' @return Returns a data.frame containing the entire genome as downloaded from UCSC
-# @import rtracklayer
-#' @export
-#' @examples
-#'    # following line not run, as there's no point
-#'    # grabbing an entire genome each time this is compiled
-#'    #db = ucscDbDump()
-#'    data(ucsc.mm9) # instead, this is equivalent.
-#'    db = ucsc.mm9
-#'    head(db)
-ucscDbDump <- function(session = NULL, genome='mm9', format = 'refGene') {
-    #require('rtracklayer')
-    if (is.null(session)) {
-        session = browserSession("UCSC")
-        genome(session) = genome
-    }
-    message("getting reference...")
-    ucsc.db = ucscTableQuery(session, format, range = genome(session))
-    message("getting table...")
-    ucsc.table = getTable(ucsc.db)
-    message("done!")
-    return(ucsc.table)
-}
+# by Sam Bassett and Ash Waardenberg, VCCRI, 2014
 
 #' @title Annotate .bed file with closest genes
-#' @description Takes coordinates from a .bed file and a data.frame of genes, then maps each coordinate to its closest gene and calculates the distance.
+#' @description Wrapper for locateVariants(). Returns a GRanges with the gene and transcript ids associated with the input .bed regions.
 #' @param path The system path to a .bed file
 #' @param bedfile If the user has a .bed file already loaded in R, they can supply it here rather than re-importing it
-#' @param db A data.frame containing the complete genome regions of the target organism, must be in the format returned by ucscDbDump
-#' @param threshold The distance threshold at which to cut genes; if a gene is further away than this, it is discounted.
-#' @details This function can take some time to run. It performs some optimisation, but it still has to search a portion of a full genome for each input coordinate. The closest gene is determined by the distance from its start point to the midpoint of the supplied coordinate. It corrects for genes on the '-' strand in this calculation as well.
+#' @param db A TranscriptDb object containing the transcripts of the organism required
 #' @export
-# @import rtracklayer
-#' @return A data.frame of the closest genes to each .bed region, plus the distance between this gene and the midpoint of the region.
+#' @return A GRanges object with corresponding EntrezGene IDs
 #' @examples
 #'   data(ucsc.mm9)
 #'   data(bed.sample)
 #'   x = annotateBedFromUCSC(bedfile = bed.sample, db = ucsc.mm9)
 #'   str(x)
-annotateBedFromUCSC <- function(path = NULL, bedfile = NULL, db = NULL, threshold = 10000) {
+annotateBedFromUCSC <- function(path = NULL, bedfile = NULL, db = NULL) {
     if (!is.null(path) && !is.null(bedfile))
         stop("Both bed and path supplied, please use only one.")
     if (is.null(path) && is.null(bedfile))
         stop("Please supply either the path to a .bed file or the raw bedfile")
-    #require('rtracklayer')
-    if(is.null(db)) {
-        message("grabbing db dump, may take time...")
-        db = ucscDbDump()
-    }
-    if (is.null(bedfile)) {
-        bed = read.bed(path)
-    } else {
-        bed = bedfile
-    }
-    # sample file
-    #bed = bed[sample(nrow(bed), 50),]
-    #bed$start = bed$start - upstream
-    #bed$end = bed$end + downstream
-    numBins = floor(max(db$txStart)/1000000)
-    message("Creating bins...")
-    binList = sapply(1:numBins, function(x) {
-        #print(paste("<", x*1000000, ", >=", (x-1)*1000000, sep=''))
-        sub = subset(db, db$txStart < x*1000000)
-        sub = subset(sub, sub$txStart >= (x-1)*1000000)
-        return(sub)
-    })
-    message("Done!")
 
-    closestGenes = data.frame()
-    start = proc.time()
-    time = vector()
-    message("Starting annotation, this process can take time.")
-    progress = txtProgressBar(min = 0, max = nrow(bed), style = 3)
-    for (j in 1:nrow(bed)) {
-        line = bed[j,]
-        if (j == 1 | j %% 10 == 0) {
-            setTxtProgressBar(progress, j)
-            currTime = proc.time() - start
-            start = proc.time()
-            time = append(time, currTime[['elapsed']])
-            avg = mean(time, na.rm=TRUE)
-        }
-        if(j == floor(nrow(bed)/2)) {
-            message()
-            message(paste("approx.", round(avg*((nrow(bed)-j)/10)/60, digits = 3), "minutes remaining", sep=' '))
-        }
-        if(j == floor(nrow(bed)/4)) {
-            message()
-            message(paste("approx.", round(avg*((nrow(bed)-j)/10)/60, digits = 3), "minutes remaining", sep=' '))
-        }
-        if(j == floor(3*nrow(bed)/4)) {
-            message()
-            message(paste("approx.", round(avg*((nrow(bed)-j)/10)/60, digits = 3), "minutes remaining", sep=' '))
-        }
-# to hopefully speed up, first subset by range, then by chr:
-        binNumber = floor(line[['start']] / 1000000)
-        db.sub = as.data.frame(binList[,binNumber])
-        if(binNumber < ncol(binList))
-            db.sub = rbind(db.sub, binList[,binNumber + 1])
-        if(binNumber > 1)
-            db.sub = rbind(db.sub, binList[,binNumber - 1])
-        db.sub = subset(db.sub, as.character(db.sub$chrom) == as.character(line[['chr']]))
-        if(nrow(db.sub) == 0)
-            next
-        peakLen = line[['end']] - line[['start']]
-        peakMid = (line[['start']] + line[['end']])/2
-        shortestLen = 999999999999
-        distances = apply(db.sub, 1, function(x) {
-            if (x[['strand']] == '+') {
-                distance = peakMid - as.numeric(x[['txStart']])
-            } else {
-                distance = as.numeric(x[['txEnd']]) - peakMid
-            }
-            return(distance)
-        })
-        if (length(distances) != nrow(db.sub)) {
-            print(length(distances))
-            print(nrow(db.sub))
-            stop("Something wrong in distance calculation")
-        }
-        minIndex = which.min(abs(distances))
-        closest = db.sub[minIndex,]
-        closest$distance = distances[minIndex]
-        closest$bedStart = line$start
-        closest$bedEnd   = line$end
-        closestGenes = rbind(closest, closestGenes)
-    }
-    closestGenes = subset(closestGenes, abs(closestGenes$distance) < threshold)
-    return(closestGenes)
-}
+    if (is.null(bedfile))
+        bed = import.bed(path)
+    else
+        if(class(bedfile) == "GRanges")
+            bed = bedfile
+        else stop("bedfile must be a GRanges object")
 
-#' @title Generates and plots a kernal density estimate of distances
-#' @description From a list of distances (such as distances of .bed regions from the start of transcription), this function generates a KDE
-#' @param distanceList A vector of distances to plot
-#' @param bw The badnwidth used by the KDE
-#' @param to The upper limit used by KDE
-#' @param from The lower limit used by KDE
-#' @param probeOverlay A vector containing the probe density so it may be compared with the distance density
-#' @export
-#' @examples
-#'   data(ucsc.mm9)
-#'   data(bed.sample)
-#'   geneList = annotateBedFromUCSC(bedfile=bed.sample, db=ucsc.mm9)
-#'   plotDistanceDistribution(geneList$distance, bw=300)
-plotDistanceDistribution <- function(distanceList, bw = NULL, to = 10000, from = -10000, probeOverlay = NULL) {
-# We got the best results with bw=300
-    p = plot(density(distanceList, bw = bw, from = from, to = to, na.rm=TRUE))
-    if(!is.null(probeOverlay))
-        p = p + lines(density(probeOverlay, from = from, to = to))
-}
-#' @title Reads in a .bed file as a data.frame
-#' @description Reads in a .bed file as a data.frame, replaces chr* with * if subChr is true (in case needed for biomaRt or something)
-#' @param path The system path to a .bed file
-#' @param subChr if true, s/chr([^)]+)/$1/
-#' @return A data.frame of the .bed coordinates
-#' @examples
-#'   ## not really relevant as system path is required
-read.bed <- function(path, subChr = FALSE) {
-    bed <- read.table(path)
-    if (ncol(bed) > 3)
-        bed = bed[,1:3]
-    if (subChr) {
-        bed$V1 = sub(pattern="chr", replacement="", bed$V1)
-    }
-    names(bed) = c('chr', 'start', 'end')
-    return(bed)
+    var = locateVariants(bed, db, IntronVariants())
+    return(var)
 }
 
 #' @title Get the functional annotation table of a gene list using DAVID
@@ -195,7 +51,7 @@ read.bed <- function(path, subChr = FALSE) {
 #'      david = DAVIDWebService$new(email = "your.registered@@email.com")
 #'      fnAnot = getFnAnot_genome(entrezList, david = david)
 #'   }
-getFnAnot_genome <- function(geneList, david = NULL, email = NULL, idType = "REFSEQ_MRNA", listName = "auto_list", rawValues = T) {
+getFnAnot_genome <- function(geneList, david = NULL, email = NULL, idType = "ENTREZ_GENE_ID", listName = "auto_list", rawValues = T) {
     #require('RDAVIDWebService')
     if (is.null(david) && !is.null(email)) {
         david <- DAVIDWebService$new(email = email)
@@ -221,30 +77,6 @@ subOntology <- function(set, ont) {
     set = DAVIDFunctionalAnnotationChart(set)
     return(set)
 }
-
-"
-#' @title Compares gene lists using DAVID's functional clustering algorithm
-#' @description Uses DAVID gene clustering (based on a Kappa statistic) to group genes and terms into strongly enriched groups then compares these groups between two initial gene sets.
-#' @export
-"
-'
-compareClusters <- function(listA, listB, david = NULL, email = NULL, listName=NULL) {
-    if(is.null(david)) {
-        david = DAVIDWebService$new(email=email)
-    }
-    if(!RDAVIDWebService::is.connected(david))
-        connect(david)
-    message("As this interfaces with DAVID online, this can take some time. Please be patient.")
-    message("Uploading setA")
-    addList(david, listA, idType=idType, listType="Gene", listName = ifelse(is.null(listName), "listA", paste(listName, "A", sep="")))
-    message("Finished uploading setA, generating report...")
-    clusterA = getClusterReport(david, type="Term")
-    message("Uploading setB")
-    addList(david, listB, idType=idType, listType="Gene", listName = ifelse(is.null(listName), "listB", paste(listName, "B", sep="")))
-    message("Finished uploading setB, generating report...")
-    clusterB = getClusterReport(david, type="Term")
-}
-'
 
 ###########################################
 ###########################################
